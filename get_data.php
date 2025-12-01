ca3c9f5d6bd9da5442ced72172094a4062947dd6
Enamul Haque <135358920+enamulhaque0448@users.noreply.github.com>
2025-12-01 13:32:21 +0600

    Add get_data.php to fetch and return readings

---
+++ get_data.php
@@ -0,0 +1,25 @@
+<?php
+include 'db_connect.php';
+
+$sql = "SELECT id, temperature, humidity, pressure, reg_date 
+        FROM readings 
+        ORDER BY id DESC 
+        LIMIT 50";
+
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+$data = array_reverse($data);
+
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
